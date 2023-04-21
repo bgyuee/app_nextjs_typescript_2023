7dda9bb6a2e2b66c6ddf3b21b4bc8c3bf7687d8b
bgyuee <127265942+bgyuee@users.noreply.github.com>
2023-04-21 13:53:33 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
-// next.config.js
-const debug = process.env.NODE_ENV !== 'production'
-const name = 'app_nextjs_typescript_2023'
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: !debug ? `${name}` : '',
-  basePath: '/app_nextjs_typescript_2023',
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성
 }
 
 module.exports = nextConfig
 
+// next.config.js
+const debug = process.env.NODE_ENV !== 'production'
+const name = 'app_nextjs_typescript_2023'
+
+module.exports = {
+  assetPrefix: !debug ? `${name}` : '',
+  basePath: '/app_nextjs_typescript_2023',
+}
